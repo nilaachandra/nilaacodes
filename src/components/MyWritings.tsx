556a289702303276e0c4bbca83990c24ed3f0d14
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-07-04 00:26:59 +0530

    Update MyWritings.tsx

--- src/components/MyWritings.tsx
+++ src/components/MyWritings.tsx
@@ -3,14 +3,14 @@ const MyWritings = () => {
     <div className="w-full">
       <h1 className="text-lg font-bold mb-3">Selected Writings</h1>
       <p className="leading-5">
-        Apologies, my dear lad, I haven&apos;t graced any writing just yet, though
+        Apologies, my dear lads, I haven&apos;t graced any writing just yet, though
         I do bless{" "}
         <a href="https://x.com/nilaacodes" className="text-blue-700 underline">
-          Twiiter
+          Twitter
         </a>{" "}
-        with my daily (and highly intellectual) shitposts.
+        with my daily (and highly intellectual) shitposts at best.
       </p>
-
+<p>There you go, I just wrote a poetry 😉</p>
       
     </div>
   );
