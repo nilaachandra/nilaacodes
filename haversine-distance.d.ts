f4cdd787bd340e7f2c84a2d7c9accc9f22bccfc5
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-07-25 20:47:24 +0530

    add

--- haversine-distance.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'haversine-distance' {
-    function haversine(start: { latitude: number; longitude: number }, end: { latitude: number; longitude: number }): number;
-    export = haversine;
-  }
