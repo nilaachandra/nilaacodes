06d8c0730acc7a95e5075cc416297536388b8542
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-08-01 15:00:33 +0530

    blog second

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "github.com",
-        },
-        {
-          protocol: "https",
-          hostname: "raw.githubusercontent.com",
-        },
-        {
-          protocol: "https",
-          hostname: "logomoji.vercel.app",
-        },
-        {
-          protocol: "https",
-          hostname: "uploads-ssl.webflow.com",
-        },
-        {
-          protocol: "https",
-          hostname: "avatars.githubusercontent.com",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "logomoji.vercel.app",
+      },
+      {
+        protocol: "https",
+        hostname: "uploads-ssl.webflow.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // Target specific routes with custom cache control
+        source: '/writings', // Adjust to match your specific page or route
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store', // Disables caching
+          },
+        ],
+      },
+      // You can add more custom headers for other routes if needed
+    ];
+  },
+};
+
+export default nextConfig;
